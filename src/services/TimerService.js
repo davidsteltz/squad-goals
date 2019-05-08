0505c81591d00da46feff73dc5e5c85966fcc2ef
Jennifer Moore <jenniferplusplus@gmail.com>
2019-05-08 01:08:21 -0500

    Timer example

---
+++ src/services/TimerService.js
@@ -0,0 +1,12 @@
+import { interval } from 'rxjs'
+
+class TimerService {
+  constructor () {
+    this.$timer = interval(100)
+  }
+}
+
+const GlobalTimer = new TimerService()
+
+export default TimerService
+export { GlobalTimer }
