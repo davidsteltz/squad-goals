1e7247cf3302ab4cd22efc6536342c1cae13eba0
shanellewebster <webstershanelle@gmail.com>
2019-07-07 20:19:57 -0500

    some styling progress

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -5,27 +5,41 @@
     </h1>
     <div class="content">
       <form name="contact">
-        <label class="form-label" for="name">
-          Name:
-        </label>
-        <input class="form-field" name="name" id="name" />
-        <label class="form-label" for="email">
-          Email:
-        </label>
-        <input class="form-field" name="email" id="email" />
-        <label class="form-label" for="password">
-          Password:
-        </label>
-        <input class="form-field" name="password" id="password" />
-        <label class="form-label" for="confirmpassword">
-          Confirm Password:
-        </label>
-        <input class="form-field" name="confirmpassword" id="confirmpassword" />
-        <label class="form-label" for="confirmpassword">
-          Over 13 years of age?:
-        </label>
-        <input type="checkbox" class="form-field" name="confirmpassword" id="confirmpassword" />
-        <input class="form-button" type="submit" value="Create Account" />
+          <div class="input-container">
+            <label class="form-label" for="name">
+            Name:
+            </label>
+            <i class="fa fa-user icon"></i>
+            <input class="form-field" name="name" id="name" />
+          </div>
+          <div class="input-container">
+            <label class="form-label" for="email">
+            Email:
+            </label>
+            <i class="far fa-envelope-open"></i>
+            <input class="form-field" name="email" id="email" />
+        </div>
+        <div class="input-container">
+            <label class="form-label" for="password">
+            Password:
+            </label>
+            <i class="fas fa-unlock"></i>
+            <input class="form-field" name="password" id="password" />
+        </div>
+        <div class="input-container">
+            <label class="form-label" for="confirmpassword">
+            Confirm Password:
+            </label>
+            <i class="fas fa-unlock"></i>
+            <input class="form-field" name="confirmpassword" id="confirmpassword" />
+        </div>
+        <div class="input-container">
+            <input type="checkbox" class="form-field" name="confirmpassword" id="confirmpassword" />
+            <label class="form-label" for="confirmpassword">
+            Over 13 years of age?:
+            </label>
+        </div>
+            <input class="form-button" type="submit" value="Create Account" />
       </form>
     </div>
   </div>
@@ -40,14 +54,24 @@ export default {};
   display: flex;
   flex-direction: column;
   width: 40%;
+  box-shadow: 5px 5px 15px 5px #c4c4c4;
+  border-radius: 5px;
 
   form {
+    text-transform: uppercase;
     display: flex;
     flex-direction: column;
+    input {
+      border: 1px solid rgb(177, 177, 177);
+      border-radius: 5px;
+    }
   }
 
   .form-button {
     margin: 10px;
+    background-color: gray;
+    color: white;
+    padding: 5px;
   }
 }
 </style>
