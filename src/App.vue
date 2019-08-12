2f766cb11f219bdfd1d7d13a215e5be9fe852f04
David Steltz <davidsteltz@users.noreply.github.com>
2019-08-12 10:02:33 -0400

    Design Changes

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,59 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/counter">Counter</router-link> |
-      <router-link to="/timer">Timer</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
-
+<template><div id="app"><div id="nav"><router-link to="/">Home</router-link>| <router-link to="/about">About</router-link>| <router-link to="/counter">Counter</router-link>| <router-link to="/timer">Timer</router-link></div><router-view/></div></template>
 <style lang="scss">
+
+//Color Variables
+$primary-color: #2c3e50;
+$secondary-color: #42b983;
+$text-color: #fff;
+//App
+body {
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  height: 100vh;
+  margin: 0;
+  background: $primary-color;
+  color: $text-color;
 }
+
 #nav {
   padding: 30px;
+
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $text-color;
+
     &.router-link-exact-active {
-      color: #42b983;
+      color: $secondary-color;
     }
   }
 }
-body, #app {
-  height: 100vh;
-  margin: 0;
-  background:#2c3e50 !important;
-  color: #eeeeee;
-}
-#nav a {
-  color: #fff;
-}
+
 button {
-    font-size: 18px;
-    margin: 5px;
-    border: 2px solid #333333;
-    padding: 10px 20px;
-    border-radius: 300px;
-    background: #eeeeee;
-    color: #333333;
-    transition: .4s;
+  font-size: 18px;
+  margin: 20px 5px;
+  border: 2px solid  rgba(255, 255, 255, .5);
+  padding: 10px 20px;
+  border-radius: 300px;
+  background: rgba(0, 0, 0, .5);
+  color: rgba(255, 255, 255, .9);
+  transition: .4s;
 }
+
 button:hover {
-  background: #dddddd;
-  color: #000000;
+  border-color: rgba(255, 255, 255, 1);
+  color: rgba(255, 255, 255, 1);
   transition: .2s;
   cursor: pointer;
 }
+
+button:focus {
+  outline: none;
+}
+
 </style>
